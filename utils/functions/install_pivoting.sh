b4110206b174c046809c57a2ec880b85f73f6fa4
giacomo.lenzini <giacomo.lenzini@securenetwork.com>
2024-06-12 15:44:47 +0200

    Added Ligolo-ng Tool

--- utils/functions/install_pivoting.sh
+++ utils/functions/install_pivoting.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+LIGOLO_VERSION="0.5.2"
+CHISEL_VERSION="1.9.1"
+
+function install_ligolo() {
+    # Ligolo
+    folder "Pivoting/Ligolo/Linux"
+    download_tool "Ligolo Proxy Linux" "https://github.com/nicocha30/ligolo-ng/releases/download/v${LIGOLO_VERSION}/ligolo-ng_proxy_${LIGOLO_VERSION}_linux_amd64.tar.gz" && tar xvf ligolo-ng_proxy_${LIGOLO_VERSION}_linux_amd64.tar.gz > /dev/null 2>&1 && rm ligolo-ng_proxy_${LIGOLO_VERSION}_linux_amd64.tar.gz
+    download_tool "Ligolo Agent Linux" "https://github.com/nicocha30/ligolo-ng/releases/download/v${LIGOLO_VERSION}/ligolo-ng_agent_${LIGOLO_VERSION}_linux_amd64.tar.gz" && tar xvf ligolo-ng_agent_${LIGOLO_VERSION}_linux_amd64.tar.gz > /dev/null 2>&1 && rm ligolo-ng_agent_${LIGOLO_VERSION}_linux_amd64.tar.gz
+
+    folder "Pivoting/Ligolo/Linux"
+    download_tool "Ligolo Agent Windows" "https://github.com/nicocha30/ligolo-ng/releases/download/v${LIGOLO_VERSION}/ligolo-ng_agent_${LIGOLO_VERSION}_windows_amd64.zip" && unzip ligolo-ng_agent_${LIGOLO_VERSION}_windows_amd64.zip > /dev/null 2>&1 && rm ligolo-ng_agent_${LIGOLO_VERSION}_windows_amd64.zip
+    download_tool "Ligolo Proxy Windows" "https://github.com/nicocha30/ligolo-ng/releases/download/v${CHISEL_VERSION}/ligolo-ng_proxy_${CHISEL_VERSION}_windows_amd64.zip" && unzip ligolo-ng_proxy_${CHISEL_VERSION}_windows_amd64.zip > /dev/null 2>&1 && rm ligolo-ng_proxy_${CHISEL_VERSION}_windows_amd64.zip
+}
+
 function install_pivoting() {
     CURRENT_COLOR=$GREEN
     #Pivoting
@@ -7,5 +21,5 @@ function install_pivoting() {
     folder "Pivoting"
 
     # Chisel
-    download_tool "Chisel" "https://github.com/jpillora/chisel/releases/download/v1.9.1/chisel_1.9.1_linux_amd64.gz" && gzip -d chisel_1.9.1_linux_amd64.gz > /dev/null 2>&1 && rm chisel_1.9.1_linux_amd64.gz
+    download_tool "Chisel" "https://github.com/jpillora/chisel/releases/download/v${CHISEL_VERSION}/chisel_${CHISEL_VERSION}_linux_amd64.gz" && gzip -d chisel_${CHISEL_VERSION}_linux_amd64.gz > /dev/null 2>&1 && rm chisel_${CHISEL_VERSION}_linux_amd64.gz
 }
