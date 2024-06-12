fa20cd4406b875a85f32dc08d53c788f7d675b8d
GiacoLenzo2109 <GiacoLenzo2109@gmail.com>
2024-06-12 21:59:18 +0200

    Add Pivoting Caterogy 🚀

--- utils/check_options.sh
+++ utils/check_options.sh
@@ -2,7 +2,7 @@
 
 # Function to verify and convert packages to array
 function verify_packages() {
-    valid_categories=("network" "web" "seclists" "kernel-exploits" "privesc" "password-crackers")
+    valid_categories=("network" "web" "seclists" "kernel-exploits" "privesc" "password-crackers" "pivoting")
     IFS=',' read -r -a PACKAGES_ARRAY <<< "$PACKAGES"
     for category in "${PACKAGES_ARRAY[@]}"; do
         if [[ ! " ${valid_categories[*]} " =~ " $category " ]]; then
