a7c0f74b95237eeb98c4836c3b3e76ce488063f6
GiacoLenzo2109 <GiacoLenzo2109@gmail.com>
2024-06-12 22:04:10 +0200

    Add Ligolo 🚀

--- utils/functions/install_pivoting.sh
+++ utils/functions/install_pivoting.sh
@@ -9,7 +9,7 @@ function install_ligolo() {
     download_tool "Ligolo Proxy Linux" "https://github.com/nicocha30/ligolo-ng/releases/download/v${LIGOLO_VERSION}/ligolo-ng_proxy_${LIGOLO_VERSION}_linux_amd64.tar.gz" && tar xvf ligolo-ng_proxy_${LIGOLO_VERSION}_linux_amd64.tar.gz > /dev/null 2>&1 && rm ligolo-ng_proxy_${LIGOLO_VERSION}_linux_amd64.tar.gz
     download_tool "Ligolo Agent Linux" "https://github.com/nicocha30/ligolo-ng/releases/download/v${LIGOLO_VERSION}/ligolo-ng_agent_${LIGOLO_VERSION}_linux_amd64.tar.gz" && tar xvf ligolo-ng_agent_${LIGOLO_VERSION}_linux_amd64.tar.gz > /dev/null 2>&1 && rm ligolo-ng_agent_${LIGOLO_VERSION}_linux_amd64.tar.gz
 
-    folder "Pivoting/Ligolo/Linux"
+    folder "Pivoting/Ligolo/Windows"
     download_tool "Ligolo Agent Windows" "https://github.com/nicocha30/ligolo-ng/releases/download/v${LIGOLO_VERSION}/ligolo-ng_agent_${LIGOLO_VERSION}_windows_amd64.zip" && unzip ligolo-ng_agent_${LIGOLO_VERSION}_windows_amd64.zip > /dev/null 2>&1 && rm ligolo-ng_agent_${LIGOLO_VERSION}_windows_amd64.zip
     download_tool "Ligolo Proxy Windows" "https://github.com/nicocha30/ligolo-ng/releases/download/v${CHISEL_VERSION}/ligolo-ng_proxy_${CHISEL_VERSION}_windows_amd64.zip" && unzip ligolo-ng_proxy_${CHISEL_VERSION}_windows_amd64.zip > /dev/null 2>&1 && rm ligolo-ng_proxy_${CHISEL_VERSION}_windows_amd64.zip
 }
